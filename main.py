eb405efd5c38f5254999c1b81e9da98895ff2a1b
joNA <jitzian@gmail.com>
2025-01-14 18:25:38 -0500

    github-actions - Details below: - Renaming 'hola-mundo.yaml' -> 'hola-mundo.yml' - Addition of triggers.yml file - Addition of env variable inside main.py file

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import os
+
 def __main__():
     print("Hello, World as a test!")
+    name = os.getenv("USERNAME")
+    print(f"Hello {name}, from github!")
 
 if __name__ == "__main__":
     __main__()
+    
